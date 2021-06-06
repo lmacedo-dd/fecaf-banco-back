e841f8fcdf04f689a283f52144097570f32affdb
Lmacedox <lm1705258@gmail.com>
2021-06-06 20:02:29 -0300

    Second Update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Serviço Iniciado")
+const express = require('express');
+const path = require('path');
+const config = require('config');
+
+const port = process.env.PORT || config.get('server.port')
+const lancamentos = [
+  {
+    data: '2021-03-10',
+    descricao: 'pagamento',
+    valor: 2500.00
+  }
+]
+
+const app = express();
+
+app.set('port', port)
+
+app.route('/extrato').get(
+  (req,res) =>{
+    res.status(200).json(lancamentos)
+  }
+)
+
+app.listen(port, ()=>{
+  console.log("Servidor iniciado")
+})
